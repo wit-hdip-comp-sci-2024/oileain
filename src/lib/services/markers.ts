35e97a36c455fa8e9e1cc57e0effe6fe8cd71d00
Eamonn de Leastar <edeleastar@wit.ie>
2025-05-05 06:51:51 +0100

    marker abstractions

---
+++ src/lib/services/markers.ts
@@ -0,0 +1,12 @@
+// import type { LatLng } from "leaflet";
+
+export interface MarkerSpec {
+  id: string;
+  title: string;
+  location: LatLng;
+}
+
+export interface MarkerLayer {
+  title: string;
+  markerSpecs: MarkerSpec[];
+}
